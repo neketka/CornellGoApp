93861988908fe66ffd0093865a43260c96cdf004
neketka <neketka@gmail.com>
2020-12-28 16:13:35 -0500

    Added example for controller.

---
+++ Backend/Controllers/AuthController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BackendModel;
+using RequestModel;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly CornellGoDb _context;
+
+        public AuthController(CornellGoDb context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<GetAuthResponse> CheckLoginStatus(GetAuthIdRequest request)
+        {
+            return new(await _context.UserSessions.FromToken(request.Session) != null);
+        }
+
+        [HttpPost]
+        public async Task<PostAuthResponse> Login(PostAuthRequest request)
+        {
+            User user = (await _context.Authenticators.FirstAsync(u => u.Username == request.Username && u.Password == request.Password))?.User;
+
+            if (user == null) // No such user exists
+                return new("");
+
+            UserSession session = await _context.UserSessions.FirstAsync(s => s.User.Id == user.Id);
+
+            if (session != null) // Session already exists
+                return new(session.ToToken());
+
+            // Create new session
+            string id = (await _context.UserSessions.AddAsync(new UserSession(DateTime.UtcNow, user))).Entity.ToToken();
+            await _context.SaveChangesAsync();
+
+            return new(id);
+        }
+
+        [HttpDelete]
+        public async Task<DeleteAuthResponse> Logout(DeleteAuthRequest request)
+        {
+            UserSession session = await _context.UserSessions.FromToken(request.Session);
+
+            if (session == null)
+                return new(false);
+
+            _context.Remove(session);
+            await _context.SaveChangesAsync();
+
+            return new(true);
+        }
+
+        [HttpGet("id")]
+        public async Task<GetAuthIdResponse> GetUserId(GetAuthIdRequest request)
+        {
+            return new((await _context.UserSessions.FromToken(request.Session))?.User.Id.ToString() ?? "");
+        }
+    }
+}
---
+++ BackendModel/UserSessionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendModel
+{
+    public static class UserSessionsExtensions
+    {
+        public static Task<UserSession> FromToken(this DbSet<UserSession> userSessions, string session)
+        {
+            return userSessions.FirstAsync(s => s.Id.ToString() == session);
+        }
+    }
+
+    public static class UserSessionExtensions
+    {
+        public static string ToToken(this UserSession userSession)
+        {
+            return userSession.ToString();
+        }
+    }
+}
