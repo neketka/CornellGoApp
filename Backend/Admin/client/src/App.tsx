2a2fd9da957b42f9d98ef7936ca4a1884371e7a7
neketka <neketka@gmail.com>
2021-03-19 16:08:12 -0400

    Cleaned up

--- Backend/Admin/client/src/App.tsx
+++ Backend/Admin/client/src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+    BrowserRouter, Switch, Route
+} from "react-router-dom";
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="subtitle1" color="inherit">
+                        CornellGO! Manager
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            
+            <Switch>
+                <Route path="/dashboard">
+                </Route>
+                <Route path="/">
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
 }
-
-export default App;
