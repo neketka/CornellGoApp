6561a7da8eeb750be07a4ba772d408e7b976e976
neketka <neketka@gmail.com>
2021-04-11 22:27:34 -0400

    Move mobile app to prod.

--- MobileApp/MobileApp/Services/GameService.cs
+++ MobileApp/MobileApp/Services/GameService.cs
@@ -33,7 +33,7 @@ namespace MobileApp.Services
 
         public GameService()
         {
-            Client = new CornellGoClient("https://10.0.2.2:5001/hub");
+            Client = new CornellGoClient("https://cornellgo.herokuapp.com/hub");
             CrossGeolocator.Current.PositionChanged += (s, e) => PollLocation(e.Position);
             CrossGeolocator.Current.DesiredAccuracy = 1;
         }
